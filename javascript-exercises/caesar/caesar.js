637f950ed2aa5529019eda9a4b8d876077defb5f
Josh <joshua.sandefer@gmail.com>
2020-05-04 07:37:30 -0500

    finished

--- javascript-exercises/caesar/caesar.js
+++ javascript-exercises/caesar/caesar.js
@@ -7,12 +7,17 @@ const caesar = function(string, shift) {
 }
 
 const getUnicode = char => char.charCodeAt();
+const modifier = charUnicode => charUnicode < 97 ? 65 : 97;
+const wrapper = (a, b) => (a % b + b) % b;
+
 
 function shiftFunction(char, shift) {
-    let charUnicode = char.charCodeAt(0);
-    let newChar = charUnicode + shift;
-    const newString = String.fromCharCode(newChar); 
-    return newString;
+    const charUnicode = char.charCodeAt(0);
+    if ((charUnicode > 64 && charUnicode < 91) || (charUnicode > 96 && charUnicode < 123)) {
+        return String.fromCharCode(
+            wrapper(charUnicode + shift - modifier(charUnicode), 26) + modifier(charUnicode)
+            ); 
+    } return char;
 }
 
-//module.exports = caesar
+module.exports = caesar
